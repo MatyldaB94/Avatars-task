c9fa0c20f1b96a0e56b4cdc353b06a34d7f75987
MatyldaB94 <matyldablakala@gmail.com>
2023-01-16 18:16:05 +0100

    refactor: add styled-components

--- src/Avatars.js
+++ src/Avatars.js
@@ -3,6 +3,25 @@ import styled from "styled-components";
 
 const StyledLoading = styled.p`
 font-size: 50px;
+width: 30px;
+`
+const StyledAvatars = styled.img`
+width: 30rem;
+height: 30rem;
+`
+const StyledButton = styled.button`
+width: 100px;
+height: 30px;
+margin: 20px 20px;
+border: 2px
+border-radius: 5px;
+ &:hover,
+  &:focus {
+    color: blue;
+  }
+  &:active {
+    color: red;
+  }
 `
 
 const Avatars = () => {
@@ -51,10 +70,10 @@ const Avatars = () => {
 
     return isLoading ? <StyledLoading>Loading...</StyledLoading> : (
         <div>
-            <img src={avatars[currentUserNameIndex]} alt={`${usernames[currentUserNameIndex]}'s avatar`} />
+            <StyledAvatars src={avatars[currentUserNameIndex]} alt={`${usernames[currentUserNameIndex]}'s avatar`}></StyledAvatars>
             <div>
-                <button onClick={onClickButtonPrevious}>Previous</button>
-                <button onClick={onClickButtonNext}>Next</button>
+                <StyledButton onClick={onClickButtonPrevious}>Previous</StyledButton>
+                <StyledButton onClick={onClickButtonNext}>Next</StyledButton>
             </div>
         </div>
     )
